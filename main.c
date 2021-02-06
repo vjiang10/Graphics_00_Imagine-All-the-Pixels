20e05b0da703c24795d3189fd85679a0c95f534f
Vincent Jiang <vincentjiang2003@gmail.com>
2021-02-06 00:44:38 -0500

    finished

--- main.c
+++ main.c
@@ -17,14 +17,14 @@ int main() {
     for (i=0; i<500; i++) {
         for (j=0; j<500; j++) {
             rgb[0] = rgb[1] = rgb[2] = 0;
-            rgb[1] = i%50;
+            rgb[1] = i%100;
             double square = (i-250)*(i-250) + (j-250)*(j-250);
-            if (square/100 > 600) {rgb[2] = (i+j)%100;}
-            else if (square/100 > 400) {rgb[2] = (i+j)%100+20;}
-            else if (square/100 >200) {rgb[2] = (i+j)%100+40;}
-            else {rgb[2] = (i+j)%100+60;}
+            if (square/100 > 600) {rgb[2] = (i+j)%200;}
+            else if (square/100 > 400) {rgb[2] = (i+j)%200+20;}
+            else if (square/100 > 200) {rgb[2] = (i+j)%200+40;}
+            else {rgb[2] = (i+j)%200+60;}
             int k;
-            for (k=0; k<3; k++) rgb[k]+=0.3*rgb[k]+(int)(square/100)%100;
+            for (k=0; k<3; k++) rgb[k]+=0.1*rgb[k]+(int)(square/100)%100;
             snprintf(line, sizeof(line), "%d %d %d ", rgb[0], rgb[1], rgb[2]);
             write(fd, line, strlen(line));
         }
